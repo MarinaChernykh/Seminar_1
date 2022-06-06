b62908ca357c2890a0e9d877727978d7a4b2358b
Marina Chernykh <danmarizza@gmail.com>
2022-06-06 22:13:59 +0300

    Задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine ("Введите трехзначное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA < 100 || numberA > 999) Console.WriteLine("Введенное число - не трехзначное");
+else 
+    Console.WriteLine (numberA%10);
