965aba557a050372fe191735a1c399dfed6e1280
Marina Chernykh <danmarizza@gmail.com>
2022-06-06 20:47:19 +0300

    Task_2 added

---
+++ Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿int DayNumber = 5;
+string Day = "Понедельник";
+if (DayNumber == 2) Day = "Вторник";
+if (DayNumber == 3) Day = "Среда";
+if (DayNumber == 4) Day = "Четверг";
+if (DayNumber == 5) Day = "Пятница";
+if (DayNumber == 6) Day = "Суббота";
+if (DayNumber == 7) Day = "Воскресенье";
+Console.WriteLine (Day);
