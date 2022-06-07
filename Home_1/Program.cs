01a476b1d402d98aef97af7a6098b5d6384d340b
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 11:56:42 +0300

    Домашнее задание, задача 2 - поиск max из 2 чисел

--- Home_1/Program.cs
+++ Home_1/Program.cs
@@ -1,20 +1,20 @@
-﻿Console.Write("Введите первое число: ");
+﻿// Задача 2.
+// Напишите программу, которая принимает на вход два числа,
+// и выдает, какое число большее
+
+Console.Write("Введите первое число: ");
 int Number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите вторе число: ");
 int Number2 = Convert.ToInt32(Console.ReadLine());
 if (Number1 > Number2)
 {
-    Console.Write("max = ");
-    Console.WriteLine (Number1);
-    Console.Write("min = ");
-    Console.WriteLine (Number2);
+    Console.Write("Большее число = ");
+    Console.WriteLine(Number1);
 }
 if (Number2 > Number1)
 {
-    Console.Write("max = ");
-    Console.WriteLine (Number2);
-    Console.Write("min = ");
-    Console.WriteLine (Number1);
+    Console.Write("Большее число = ");
+    Console.WriteLine(Number2);
 }
 if (Number1 == Number2)
-    Console.WriteLine ("Числа равны");
+    Console.WriteLine("Числа равны");
