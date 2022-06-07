b9957927b72b705d3088ff75d24c4614bd6c5d07
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 10:43:13 +0300

    Homework1 added

---
+++ Home_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+if (Number1 > Number2)
+{
+    Console.Write("max = ");
+    Console.WriteLine (Number1);
+    Console.Write("min = ");
+    Console.WriteLine (Number2);
+}
+if (Number2 > Number1)
+{
+    Console.Write("max = ");
+    Console.WriteLine (Number2);
+    Console.Write("min = ");
+    Console.WriteLine (Number1);
+}
+if (Number1 == Number2)
+    Console.WriteLine ("Числа равны");
