08cac09305b47c52d860eafe288fa1851d11ef45
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 10:43:49 +0300

    Homework2 added

---
+++ Home_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+int max = Number1;
+if (Number2 > max) max = Number2;
+if (Number3 > max) max = Number3;
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
+
