41a3080ab638c101b318800cd724f767e1ea8301
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 11:59:24 +0300

    Семинар, квадрат числа

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,10 +1,9 @@
-﻿int numberA = -3;
-int numberB = 9;
-if (numberA == numberB*numberB)
-    Console.WriteLine ("да");
+﻿//Напишите программу, которая на вход принимает 2 числа и проверяет, 
+//является ли первое число квадратом второго
 
-else
-    Console.WriteLine ("нет");
-
-
-
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB) Console.WriteLine("Первое число = квадрату второго");
+else Console.WriteLine("Первое число - не квадрат второго");
