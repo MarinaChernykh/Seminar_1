1d86d359921aa2061c8ae689a0b6461fb0890961
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 10:44:18 +0300

    Homework4 added

---
+++ Home_4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 1)
+{
+    int i = 2;
+    while (i <= N)
+    {
+        Console.Write($"{i} ");
+        i += 2;
+    }
+}
+if (N < -1)
+{
+    int i = -2;
+    while (i >= N)
+    {
+        Console.Write($"{i} ");
+        i -= 2;
+    }
+}
+if (N<=1 && N>=-1)
+{
+    Console.WriteLine("Четных чисел нет");
+}
