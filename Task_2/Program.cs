7a8ce8412262d07b274d6c679a7247cc44ad0c30
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 12:00:00 +0300

    Семинар, дни недели

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,10 +1,14 @@
-﻿int DayNumber = 9;
-string Day = "Понедельник";
-if (DayNumber < 1 || DayNumber > 7) Day = "Ошибка ввода";
-if (DayNumber == 2) Day = "Вторник";
-if (DayNumber == 3) Day = "Среда";
-if (DayNumber == 4) Day = "Четверг";
-if (DayNumber == 5) Day = "Пятница";
-if (DayNumber == 6) Day = "Суббота";
-if (DayNumber == 7) Day = "Воскресенье";
-Console.WriteLine (Day);
+﻿//Напишите программу, которая будет выдавать название дня недели 
+// по заданному номеру: 3 -> Среда
+
+
+Console.Write("Введите номер для недели: ");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day == 1) Console.WriteLine("Понедельник");
+if (Day == 2) Console.WriteLine("Вторник");
+if (Day == 3) Console.WriteLine("Среда");
+if (Day == 4) Console.WriteLine("Четверг");
+if (Day == 5) Console.WriteLine("Пятница");
+if (Day == 6) Console.WriteLine("Суббота");
+if (Day == 7) Console.WriteLine("Воскресенье");
+if (Day < 1 || Day > 7) Console.WriteLine("Ошибка ввода");
