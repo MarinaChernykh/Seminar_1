133fb188ba65bee7bd57c730aade300774d5d75f
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 11:58:32 +0300

    Домашнее задание, задача 8 - вывод четных

--- Home_4/Program.cs
+++ Home_4/Program.cs
@@ -1,24 +1,22 @@
-﻿Console.Write("Введите число N: ");
+﻿// Задача 8
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 if (N > 1)
 {
-    int i = 2;
-    while (i <= N)
+    for (int i = 2; i <= N; i += 2)
     {
         Console.Write($"{i} ");
-        i += 2;
     }
 }
+
 if (N < -1)
 {
-    int i = -2;
-    while (i >= N)
+    for (int i = -2; i >= N; i -= 2)
     {
         Console.Write($"{i} ");
-        i -= 2;
     }
 }
-if (N<=1 && N>=-1)
-{
-    Console.WriteLine("Четных чисел нет");
-}
+if (N <= 1 && N >= -1) Console.WriteLine("Четных чисел нет");
