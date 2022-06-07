f4e9a85626655a75a14ef8a95586d8290367c27a
Marina Chernykh <danmarizza@gmail.com>
2022-06-07 12:00:39 +0300

    Семинар, от -N до N

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,8 +1,13 @@
-﻿int number1 = 5;
-int i = number1 * (-1);
-while (i <= N)
+﻿//Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N
+//4 -> -4 -3 -2 -1 0 1 2 3 4 
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = -N; i <= N; i++)
 {
-    Console.Write (i);
-    i++;
+    Console.Write(i);
+    Console.Write(" ");
+    //также можно написать код вывода в строку с пробелом короче: Console.Write($"{i} ");
 }
 
